fa37eae207e4decc35f72549b5c681c51f2b7773
Carlson Sharpless <cwsharpless@gmail.com>
2020-09-25 15:59:56 -0400

    Created base ASP.NET system

---
+++ PickUpOrder/Controllers/IndexController.cs
@@ -0,0 +1,35 @@
+﻿using MyMvcApplication.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MyMvcApplication.Controllers
+{
+	public class IndexController : Controller
+	{
+		public ActionResult Index()
+		{
+			List<IndexModel> newList = new List<IndexModel>();
+
+			IndexModel newIndexModel = new IndexModel
+			{
+				ID = 1,
+				Description = "First Item",
+				Comments = "This is the first instance of the Model"
+			};
+
+			IndexModel newIndexModel2 = new IndexModel
+			{
+				ID = 2,
+				Description = "Second Item",
+				Comments = "This is the second instance of the Model"
+			};
+
+			// Add the items to the list
+			newList.Add(newIndexModel);
+			newList.Add(newIndexModel2);
+
+			// Send the model to view.
+			return View(newList);
+		}
+	}
+}
---
+++ PickUpOrder/Models/IndexModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace MyMvcApplication.Models
+{
+    public class IndexModel
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public string Comments { get; set; }
+    }
+}
